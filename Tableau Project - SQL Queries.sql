82316b6c4d4953b80557f4708b6f4d591ebce5e9
Andrea Gómez <88553215+AndreaGomezYn@users.noreply.github.com>
2022-09-12 16:35:08 -0500

    Add files via upload

---
+++ Tableau Project - SQL Queries.sql
@@ -0,0 +1,54 @@
+
+/*
+Queries used for Tableau Project
+*/
+
+
+-- 1. Global Numbers - Death %
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+where continent is not null 
+--Group By date
+order by 1,2
+
+-- The Second includes "International"  Location
+--Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+--From PortfolioProject..CovidDeaths
+----Where location like '%states%'
+--where location = 'World'
+----Group By date
+--order by 1,2
+
+
+-- 2. Total Deaths per continent.
+
+-- For consistency purposes, we take out data not labeled as a specific continent as they are not included in the above queries.
+-- European Union is part of Europe
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Where continent is null 
+and location not in ('World', 'European Union', 'International','Upper middle income','High income','Lower middle income','Low income')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3. Percentage Population Infected Per Country
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%states%'
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4. Percentage of Population Infected (Time Series)
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount, MAX((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+--Where location like '%United Kingdom%'
+Group by Location, Population, date
+order by PercentPopulationInfected desc
